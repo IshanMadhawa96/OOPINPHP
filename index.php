7f11f588d7f4063063113b35e35c7186d5a0bcf7
unknown <ishanmadhawa09@gmail.com>
2022-08-17 23:10:52 +0530

    class and object

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+class Student{
+  public  $names = 'Ishan';
+  public   $attandance = true;
+  public $totalMarks = 89;
+
+
+}
+
+// instance
+$student = new Student();
+echo $student->names;
+?>
