88b899a5d4651de18d0ec8380f7b3f2544709216
unknown <ishanmadhawa09@gmail.com>
2022-08-17 23:21:21 +0530

    class properties and object finished

--- index.php
+++ index.php
@@ -9,6 +9,28 @@ class Student{
 }
 
 // instance
+echo'<br>';
+echo'<br>';
+echo 'Student 1';
+echo'<br>';
+echo'<br>';
 $student = new Student();
 echo $student->names;
+echo'<br>';
+echo $student->attandance;
+echo'<br>';
+echo $student->totalMarks=100;
+// reusable single class in diffrent student
+echo'<br>';
+echo'<br>';
+echo 'Student 2';
+echo'<br>';
+echo'<br>';
+
+$student = new Student();
+echo $student->names = "Dane";
+echo'<br>';
+echo $student->attandance;
+echo'<br>';
+echo $student->totalMarks=50;
 ?>
